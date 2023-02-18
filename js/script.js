5aa18bf1cb34ef312f9c1ab7af36336d3647a3b8
daisuke <eggk@mvc.biglobe.ne.jp>
2023-02-18 12:41:46 +0900

    2/18 12:41

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 jQuery(function() {
 
 	$('.menu-btn').click(function() {
-		$('header').toggleClass('fixed');
+		$('body').toggleClass('fixed');
 		$('.menu-btn__line01').toggleClass('open');
 		$('.menu-btn__line02').toggleClass('open');
 		$('.menu-btn__line03').toggleClass('open');
@@ -12,7 +12,7 @@ jQuery(function() {
 	$(window).on('resize', function() {
 		if (window.matchMedia("(min-width: 769px)").matches) {
 	    //画面横幅が768px以下のときの処理
-			$('header').removeClass('fixed');
+			$('body').removeClass('fixed');
 	    $('.menu-btn__line01').removeClass('open');
 		$('.menu-btn__line02').removeClass('open');
 		$('.menu-btn__line03').removeClass('open');
@@ -21,5 +21,39 @@ jQuery(function() {
 	  } 
 	});
 	  
+	let targets = document.querySelectorAll('.js-slidein-bottom');
+	$(window).on('scroll load', function() {
+		var scroll = window.scrollY; //スクロール量を取得
+	  	var windowHeight = window.innerHeight; //画面の高さを取得
+	  	for (let target of targets) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+	    var targetPos = target.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
+	    if (scroll > targetPos - windowHeight + 200) { //スクロール量 > ターゲット要素の位置
+	      target.classList.add('slideinbottom'); //is-animatedクラスを加える
+	    }
+	  }
+	});
 
+	let targets_fadein = document.querySelectorAll('.js-fadein');
+	$(window).on('scroll load', function() {
+		var scroll = window.scrollY; //スクロール量を取得
+	  	var windowHeight = window.innerHeight; //画面の高さを取得
+	  	for (let target of targets_fadein) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+	    var targetPos = target.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
+	    if (scroll > targetPos - windowHeight + 200) { //スクロール量 > ターゲット要素の位置
+	      target.classList.add('fadein'); //is-animatedクラスを加える
+	    }
+	  }
+	});
+
+	let targets_slidein = document.querySelectorAll('.js-slidein');
+	$(window).on('scroll load', function() {
+		var scroll = window.scrollY; //スクロール量を取得
+	  	var windowHeight = window.innerHeight; //画面の高さを取得
+	  	for (let target of targets_slidein) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+	    var targetPos = target.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
+	    if (scroll > targetPos - windowHeight + 200) { //スクロール量 > ターゲット要素の位置
+	      target.classList.add('slidein'); //is-animatedクラスを加える
+	    }
+	  }
+	});
 });
